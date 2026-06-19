de8300828d427f3c8a1708c31251f919d8df88f7
ritika dixit <ritikadixit@ritikas-MacBook-Air-2.local>
2026-06-20 02:23:31 +0530

    Add config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// Roro API Keys — DO NOT commit this file to GitHub
+// This file is listed in .gitignore
+window.RORO_CONFIG = {
+  GROQ_KEY:            "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",
+  AZURE_KEY:           "EsDcrvARbl73OmASgYJdoF7mtVPNzO2PEq2IIBRjpetXcL7MrUDTJQQJ99CFACqBBLyXJ3w3AAAYACOGMGCs",
+  AZURE_REGION:        "southeastasia",
+  EMAILJS_PUBLIC_KEY:  "Sn2xPZKuAhKed-K8g",
+  EMAILJS_SERVICE_ID:  "roro_poro",
+  EMAILJS_TEMPLATE_ID: "template_lnvddp9"
+};
